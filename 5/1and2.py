85679064a5a8960bc649aa091368c297b16446c0
tomdenboon <tomdenboon@hotmail.com>
2021-12-05 13:24:35 +0100

    solution day 5

---
+++ 5/1and2.py
@@ -0,0 +1,42 @@
+file = open('input')
+lines = {}
+for l in file:
+    tokens = l.split(' ')
+    begin_x, begin_y = [int(x) for x in tokens[0].split(',')]
+    end_x, end_y = [int(x) for x in tokens[2].split(',')]
+    x_dir = 1
+    y_dir = 1
+    if begin_y > end_y:
+        y_dir = -1
+    if begin_x > end_x:
+        x_dir = -1
+    if begin_x == end_x:
+        while begin_y != end_y + y_dir:
+            if (begin_x, begin_y) in lines:
+                lines[(begin_x, begin_y)] += 1
+            else:
+                lines[(begin_x, begin_y)] = 1
+            begin_y += y_dir
+    elif begin_y == end_y:
+        while begin_x != end_x + x_dir:
+            if (begin_x, begin_y) in lines:
+                lines[(begin_x, begin_y)] += 1
+            else:
+                lines[(begin_x, begin_y)] = 1
+            begin_x += x_dir
+    elif abs(end_x - begin_x) == abs(end_y - begin_y):
+        loops = abs(end_x - begin_x)
+        for i in range(loops+1):
+            if (begin_x, begin_y) in lines:
+                lines[(begin_x, begin_y)] += 1
+            else:
+                lines[(begin_x, begin_y)] = 1
+            begin_x += x_dir
+            begin_y += y_dir
+
+
+score = 0
+for x in lines:
+    if lines[x] >= 2:
+        score += 1
+print(score)
