73e6326e86664d410ef84f152f5a7ac365b599ca
tomdenboon <tomdenboon@hotmail.com>
2021-12-11 12:41:27 +0100

    day 11

--- 9/1and2.py
+++ 9/1and2.py
@@ -1,47 +1,38 @@
-is_visited = {}
+octopus = []
 
 
-def find_basin_size(grid, i, j):
-    if (i, j) in is_visited or i >= len(grid) or i < 0 or j >= len(grid[i]) or j < 0 or grid[i][j] == 9:
-        return 0
-    else:
-        is_visited[(i, j)] = True
-        size = 1
-        size += find_basin_size(grid, i + 1, j)
-        size += find_basin_size(grid, i - 1, j)
-        size += find_basin_size(grid, i, j + 1)
-        size += find_basin_size(grid, i, j - 1)
-        return size
+def octopus_flashbanger():
+    is_synced = True
+    for i in range(len(octopus)):
+        for j in range(len(octopus[i])):
+            octopus[i][j] = octopus[i][j] % 10
+            if octopus[i][j] != 0:
+                is_synced = False
+    return is_synced
+
+
+def add(i, j):
+    if 0 <= i < len(octopus) and 0 <= j < len(octopus[i]) and octopus[i][j] <= 9:
+        octopus[i][j] += 1
+        if octopus[i][j] > 9:
+            return 1 + add(i+1, j) + add(i+1, j+1) + add(i+1, j-1) + add(i-1, j) + add(i-1, j+1) + add(i-1, j-1) + add(i, j+1) + add(i, j-1)
+    return 0
 
 
 file = open('input')
-grid = []
 for line in file:
     line = line.strip()
-    row = []
-    print(line)
-    for x in line:
-        row.append(int(x))
-    grid.append(row)
+    octopus.append([int(c) for c in line])
 
-risk_level = 0
-big_basins = [0, 0, 0]
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        is_low = True
-        if i + 1 < len(grid) and grid[i + 1][j] <= grid[i][j]:
-            is_low = False
-        if i - 1 >= 0 and grid[i - 1][j] <= grid[i][j]:
-            is_low = False
-        if j + 1 < len(grid[i]) and grid[i][j + 1] <= grid[i][j]:
-            is_low = False
-        if j - 1 >= 0 and grid[i][j - 1] <= grid[i][j]:
-            is_low = False
-        if is_low:
-            risk_level += 1+grid[i][j]
-            size = find_basin_size(grid, i, j)
-            if size > big_basins[0]:
-                big_basins[0] = size
-            big_basins.sort()
-print(risk_level)
-print(big_basins[0] * big_basins[1] * big_basins[2])
+flash = 0
+is_synced = False
+step = 0
+while not is_synced:
+    for i in range(len(octopus)):
+        for j in range(len(octopus[i])):
+            flash += add(i, j)
+    is_synced = octopus_flashbanger()
+    step += 1
+    if step == 100:
+        print(flash)
+print(step)
