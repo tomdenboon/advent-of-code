9ee9422acf28e916550fd1c37e704bf8714d2821
tomdenboon <tomdenboon@hotmail.com>
2021-12-12 14:45:48 +0100

    day 12

---
+++ 12/2.py
@@ -0,0 +1,27 @@
+
+def dfs(node, graph, visited, twice):
+    if node == 'end':
+        return 1
+    if node.islower():
+        if node not in visited:
+            visited[node] = True
+        elif not twice and node != 'start':
+            twice = True
+        else:
+            return 0
+    reached_end = 0
+    for next_node in graph[node]:
+        reached_end += dfs(next_node, graph, visited.copy(), twice)
+    return reached_end
+
+
+graph = {}
+for line in open('input'):
+    node1, node2 = line.strip().split('-')
+    if node1 not in graph:
+        graph[node1] = []
+    if node2 not in graph:
+        graph[node2] = []
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+print(dfs('start', graph, {}, [], False))
--- README.md
+++
@@ -1,3 +0,0 @@
-# Advent of Code 2021
-
-My attempts for the Advent of Code, 2021
