da951c64a6f6841973ff512e14f8cf9e2669e963
tomdenboon <tomdenboon@hotmail.com>
2021-12-13 14:01:13 +0100

    clean up

--- 13/1and2.py
+++ 13/1and2.py
@@ -10,34 +10,18 @@ def print_paper(paper, x, y):
 
 
 def fold_x(paper, at_x):
-    delete_list = []
-    add_list = []
-    for (x, y) in paper:
-        delete_list.append((x, y))
-        if x < at_x:
-            add_list.append((x, y))
-        elif at_x - (x - at_x) >= 0:
-            add_list.append((at_x - (x - at_x), y))
-    for (x, y) in delete_list:
-        del paper[(x, y)]
-    for (x, y) in add_list:
-        paper[(x, y)] = True
+    for (x, y) in paper.copy():
+        if not x < at_x and at_x - (x - at_x) >= 0:
+            paper[(at_x - (x - at_x), y)] = True
+            del paper[(x, y)]
     return paper
 
 
 def fold_y(paper, at_y):
-    delete_list = []
-    add_list = []
-    for (x, y) in paper:
-        delete_list.append((x, y))
-        if y < at_y:
-            add_list.append((x, y))
-        elif at_y - (y - at_y) >= 0:
-            add_list.append((x, at_y - (y - at_y)))
-    for (x, y) in delete_list:
-        del paper[(x, y)]
-    for (x, y) in add_list:
-        paper[(x, y)] = True
+    for (x, y) in paper.copy():
+        if not (y < at_y) and at_y - (y - at_y) >= 0:
+            paper[(x, at_y - (y - at_y))] = True
+            del paper[(x, y)]
     return paper
 
 
