03a693c886c5ad5702046ad0dd321d75dd762c10
tomdenboon <tomdenboon@hotmail.com>
2024-12-01 10:46:28 +0100

    complete day 1

---
+++ 2024/1/solution.py
@@ -0,0 +1,26 @@
+def part_one(input_str: str):
+    left, right = parse_input(input_str)
+
+    left.sort()
+    right.sort()
+
+    return sum([abs(l - r) for (l, r) in zip(left, right)])
+        
+def part_two(input_str: str):
+    left, right = parse_input(input_str)
+
+    righ_histogram = {}
+    for r in right:
+        righ_histogram[r] = righ_histogram.get(r, 0) + 1
+
+    return sum([righ_histogram[l] * l for l in left if l in righ_histogram])
+
+def parse_input(input_str):
+    left = []
+    right = []
+    for line in input_str.split("\n"):
+        l, r = [int(x) for x in line.split()]
+        left.append(l)
+        right.append(r)
+
+    return left, right
---
+++ 2024/1/tests.py
@@ -0,0 +1,13 @@
+examples = [ 
+"""3   4
+4   3
+2   5
+1   3
+3   9
+3   3""" ]
+
+def part_one():
+    return [(examples[0], 11)]
+    
+def part_two():
+    return [(examples[0], 31)]
